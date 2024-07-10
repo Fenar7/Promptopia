b12c31ccb57df53932158e0568ec7f0dfac962e3
Fenar Khan <fenarkhan@gmail.com>
2024-07-10 07:00:13 +0530

    5

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -7,10 +7,12 @@ import {useState, useEffect} from 'react'
 import {signIn, signOut, useSesssion, getProviders} from 'next-auth/react'
 
 const Nav = () => {
+  const isUserLoggedIn = true
+
   return (
     <div>
       <nav className="flex-between w-full mb-16 pt-3">
-        <Link href="/" className="flex gap-2 flex-center">
+        <Link href="/" className="flex gap-2 flex-start">
             <Image
               src="/assets/images/logo.svg"
               alt="Promptopia logo"
@@ -18,7 +20,20 @@ const Nav = () => {
               height={30}
               className="object-contain"
             />
+            <p className="logo_text">Promptopia</p>
         </Link>
+
+        <div className="sm:flex hidden">
+          {isUserLoggedIn ? (
+            <div className="flex gap-3 md:gap-5 black_btn">
+
+            </div>
+          ):(
+            <>
+
+            </>
+          )}
+        </div>
       </nav>
     </div>
   )
