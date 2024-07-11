822c94ded99e410b7ff377eb320e75c24f0fd711
Fenar Khan <fenarkhan@gmail.com>
2024-07-11 13:52:54 +0530

    8

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,8 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 
+import { connectToDB } from '@utils/database'
+
 const handler = NextAuth({
     providers: [
         GoogleProvider({
@@ -15,9 +17,12 @@ const handler = NextAuth({
 
     async signIn({profile}){
         try{
+            await connectToDB()
 
+            return true
         } catch(error){
-            
+            console.log(error)
+            return false
         }
     }
 })
